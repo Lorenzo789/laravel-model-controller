8dadd228fd479df01222fe94230e83b1f3adc925
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-09-08 15:54:03 +0200

    adjust cotroller

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Movie;
 use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
     //
+    public function index(){
+
+       $movies = Movie::all();
+       
+        return view('movies.movie', compact('movies'));
+    }
 }
