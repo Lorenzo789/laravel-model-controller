a7efa26f12fbc99cdaa6fa40a2bc6fb1c6eb0cac
Lorenzo789 <pallozzilorenzo@gmail.com>
2022-09-08 15:54:28 +0200

    create various blade

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <nav>
+        <ul>
+            <a href="{{ route('homepage') }}"><li>Home</li></a>
+            <a href="{{ route('movie') }}"><li>Movie</li></a>
+        </ul>
+    </nav>
+</header>
